ed39d3cdef5334f2a1e237035ef8a49675796740
Kevin Koehler <kevin.koehler@usask.ca>
2020-02-14 09:31:36 -0600

    Update ast.cc

--- src/parse/ast.cc
+++ src/parse/ast.cc
@@ -3,7 +3,7 @@
 using std::unique_ptr;
 
 unique_ptr<AstNode> AstNode::make(const Symbol sym) {
-  return std::make_unique<AstNode>((AstNode *)malloc(sizeof(void *)));
+  return std::make_unique<AstNode>((AstNode *)malloc(sizeof(AstNode *)));
 }
 
 void AstNode::add_left(const Symbol sym) { this->left = AstNode::make(sym); }
